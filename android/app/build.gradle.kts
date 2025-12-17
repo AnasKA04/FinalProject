5a86a87fcc96c72b144a9e99840ef8afbd75619b
Anas Ayasrah <anasayasrah2829@gmail.com>
2025-12-17 13:10:30 +0300

    Initial Flutter project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,14 @@ android {
     }
 
     defaultConfig {
-        manifestPlaceholders["applicationName"] = "io.flutter.app.FlutterApplication"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.gp1v1"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
